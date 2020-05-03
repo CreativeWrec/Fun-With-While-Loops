98b56e536235458943516bd95c4738c64e2512dc
CreativeWrec <58211428+CreativeWrec@users.noreply.github.com>
2020-05-03 14:41:50 -0500

    Intial_Commit_&_Added_User_Input

---
+++ Fun-With-While-Loops/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Fun_With_While_Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+            int counter = 5;
+
+            while(counter < 25)
+            {
+                counter++;
+                userInput = Console.ReadLine();
+            }
+        }
+    }
+}
